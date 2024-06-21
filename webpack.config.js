c5d5ebf4ea9925177665fb0cfa07bf5490f8fc37
Henry J Ventura G <henry.ventura14@outlook.es>
2024-06-21 17:14:53 -0500

    initial commit
    Config webpack , config heroku

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',  
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
