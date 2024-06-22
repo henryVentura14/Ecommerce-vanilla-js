3eb5d6348695e801ce4ded93f4851f39659684c5
Henry J Ventura G <henry.ventura14@outlook.es>
2024-06-22 13:29:53 -0500

    update dev structure
    update dev structure, a carousel component

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackConfig = require('./webpack.config.js');
+
 const app = express();
+const compiler = webpack(webpackConfig);
 
-const PORT = process.env.PORT || 3000;
+app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath
+}));
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(webpackHotMiddleware(compiler));
 
-app.get('*', (req, res) => {
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
---
+++ src/js/constants.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://gradistore-spi.herokuapp.com';
