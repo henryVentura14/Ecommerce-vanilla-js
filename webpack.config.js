196ab66a01d1f13733288e447a94acb54944d794
henryVentura14 <henry.vetura14@gmail.com>
2026-04-16 22:23:41 -0500

    [Feature]: fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'app.bundle.js',
+        clean: true,
     },
     module: {
         rules: [
@@ -13,14 +16,46 @@ module.exports = {
                 test: /\.s[ac]ss$/i,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico|woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: (pathData) => {
+                        // Preserva la estructura original desde 'src/'
+                        const relativePath = path.relative(
+                            path.resolve(__dirname, 'src'),
+                            pathData.filename
+                        );
+                        return relativePath;
+                    }
+                }
+            },
         ],
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { 
+                    from: 'src/assets', 
+                    to: 'assets',
+                    noErrorOnMissing: true,
+                    // Evitamos sobreescribir si Webpack ya procesó el archivo como asset
+                    force: false,
+                },
+            ],
+        }),
     ],
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
         compress: true,
         port: 9000,
+        hot: true,
     },
 };
