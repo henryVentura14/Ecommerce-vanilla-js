03cd3b5bacd2a1fc56c6a495ba25d10ad1c53c96
Henry J Ventura G <31010969+henryVentura14@users.noreply.github.com>
2026-04-16 21:33:00 -0500

    Update webpack.config.js with new configuration including JSON rules, clean output, and devServer config

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,28 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./src/js/index.js",
-  output: {
-    filename: "app.bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/[name][ext]",
-        },
-      },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.json$/,
+                use: 'json-loader',
+                type: 'javascript/auto',
+            },
+            // other rules can be added here
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: "src/assets", to: "assets" }],
-    }),
-  ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
 };
